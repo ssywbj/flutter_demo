89ea8843d1b7ee476ebfd6ea78692ffbf369ae19
ssywbj <weibj@zhipu-inc.com>
2021-02-19 16:57:19 +0800

    带参命名路由传值、onGenerateRoute、显示上个路由的返回值

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,40 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(title: 'Flutter Demo Home Page'),
       //命名路由使用步骤：1.注册路由表
       routes: {
-        'home_route': (context) => MyHomePage(title: 'Flutter Demo Home Page'),
+        'home_route': (context) => MyHomePage(title: 'Flutter Demo Home Route'),
         'new_route': (context) => NewRoute(),
         'setting_route': (context) => RouteSetting(),
+        'tip_route': (context) => TipRoute(
+              //适配带参命名路由
+              text: ModalRoute.of(context).settings.arguments,
+            ),
       },
       //名为"home_route"的路由作为应用的home(首页)，会覆盖'home'属性
       initialRoute: 'home_route',
       //名为"new_route"的路由作为应用的home(首页)，会覆盖'home'属性
       //initialRoute: 'new_route',
+
+      //onGenerateRoute属性在打开命名路由时可能会被调用：当调用Navigator.pushNamed(...)打开命名路由时，
+      //如果指定的路由名在路由表中已注册，则会调用路由表中的builder函数来生成路由组件；如果路由表中没有注册，
+      //才会调用onGenerateRoute来生成路由。注意，onGenerateRoute只会对命名路由生效。
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(builder: (context) {
+          String routeName = settings.name;
+          print('+++++onGenerateRoute: $routeName');
+          //如果访问的路由页需要登录，但当前未登录，则直接返回登录页路由，引导用户登录；其它情况则正常打开路由。
+          return NewRoute();
+        });
+      },
+
+      //onUnknownRoute属性：打开一个不存在的命名路由时会被调用。但若同时存在onGenerateRoute属性和
+      //onUnknownRoute属性，那么onUnknownRoute属性会失效。
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(builder: (context) {
+          String routeName = settings.name;
+          print('-----onUnknownRoute: $routeName');
+          return TipTestRoute();
+        });
+      },
     );
   }
 }
@@ -149,17 +175,42 @@ class _MyHomePageState extends State<MyHomePage> {
             FlatButton(
               //命名路由使用步骤：2.使用路由名打开新路由
               //onPressed: () => Navigator.pushNamed(context, 'setting_route',arguments: 'hi'),
-              onPressed: () => Navigator.of(context)
-                  .pushNamed('setting_route', arguments: '值是通过命名路由传过来的'),
+              onPressed: () =>
+                  Navigator.of(context)
+                      .pushNamed('setting_route', arguments: '值是通过命名路由传过来的'),
               child: Text('路由名打开新界面并传值'),
               color: Colors.red,
               textColor: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () async {
+                var result = await Navigator.pushNamed(
+                    context, 'tip_route', arguments: '值通过路由名带参传过来');
+                print('named_tip_route, return value: $result');
+              },
+              child: Text('路由名带参并传值'),
+              color: Colors.blue,
+              textColor: Colors.black,
+            ),
+            FlatButton(
+              onPressed: () =>
+                  Navigator.pushNamed(context, 'test_OnGenerateRoute')
+              ,
+              child: Text('OnGenerateRoute'),
+              color: Colors.green,
+              textColor: Colors.black,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        //onPressed: _incrementCounter,
+        onPressed: () {
+          //或这样写：调用匿名方法
+          setState(() {
+            _counter++;
+          });
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
@@ -211,7 +262,7 @@ class TipRoute extends StatelessWidget {
   }
 }
 
-class TipTestRoute extends StatelessWidget {
+/*class TipTestRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -229,6 +280,40 @@ class TipTestRoute extends StatelessWidget {
           print('route return result: $result');
         },
         child: Text("打开新页面"),
+      ),
+    );
+  }
+}*/
+
+class TipTestRoute extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TipTestRouteState();
+  }
+}
+
+class TipTestRouteState extends State<TipTestRoute> {
+  var result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () async {
+          //打开TipRoute并等待返回结果
+          result = await Navigator.push(
+            //若不使用关键字await，则直接返回结果，此时打印的是一个对象
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      TipRoute(
+                        text: "值由上个路由带过来", //给TipRoute传值
+                      )));
+
+          print('route return result: $result');
+          setState(() {});
+        },
+        child: Text("打开新页面，返回结果：$result"),
       ),
     );
   }
@@ -260,3 +345,9 @@ class RouteSetting extends StatelessWidget {
     );
   }
 }
+
+//使用命名路由的好处
+//1.语义化更明确。
+//2.代码更好维护；如果使用匿名路由，则必须在调用Navigator.push的地方创建新路由页，
+//这样不仅需要import新路由页的dart文件，而且这样的代码将会非常分散。
+//3.可以通过onGenerateRoute做一些全局的路由跳转前置处理逻辑。
